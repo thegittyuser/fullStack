3bd1b5913f33bd87cd3147150ca4f298078a4f8c
thegittyuser <webdevsserv@gmail.com>
2025-11-24 19:31:47 +0500

    DUBARA PUSH

--- server/api/productsApi.js
+++ server/api/productsApi.js
@@ -3,25 +3,25 @@ const products = [
     id: 1,
     title: "3 Piece Khaddar Suit",
     price: "Rs. 6,999",
-    image: "./products_images/pro1.webp",
+    image: "http://localhost:5000/pro1.webp",
   },
   {
     id: 2,
     title: "3 Piece Cotton Suit",
     price: "Rs. 4,999",
-    image: "./products_images/pro2.webp",
+    image: "http://localhost:5000/pro2.webp",
   },
   {
     id: 3,
     title: "Silk Shirt (Pret)",
     price: "Rs. 5,999",
-    image: "./products_images/pro3.webp",
+    image: "http://localhost:5000/pro3.webp",
   },
   {
     id: 4,
     title: "3 Piece Cotton Suit (Pret)",
     price: "Rs. 6,999",
-    image: "./products_images/pro4.webp",
+    image: "http://localhost:5000/pro4.webp",
   },
 ];
 
